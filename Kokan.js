6faac66f7881d8b149451abad772a4e3963c7e4a
Sourabh-513 <54619080+Sourabh-513@users.noreply.github.com>
2022-06-04 02:25:56 +0530

    Update and rename Kokan.js to style.scss

--- Kokan.js
+++
@@ -1,6 +0,0 @@
-<p class="codepen" data-height="300" data-theme-id="dark" data-default-tab="js,result" data-slug-hash="YPOBEJ" data-user="ozzie" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
-  <span>See the Pen <a href="https://codepen.io/ozzie/pen/YPOBEJ">
-  Audio Player</a> by Ozzie Kirkby (<a href="https://codepen.io/ozzie">@ozzie</a>)
-  on <a href="https://codepen.io">CodePen</a>.</span>
-</p>
-<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
